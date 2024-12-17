8e8ef2594067973a7687e647d4e99667c9ffc7f5
ranjithgudesa <101344279+ranjithgudesa@users.noreply.github.com>
2024-12-17 13:56:28 +0530

    Update module.tf

--- test/module.tf
+++ test/module.tf
@@ -1,8 +1,11 @@
 module "s3" {
     source = "../modules/s3"
     s3_bucket_name = "mytest_terraform_bucket"  
+    workspace = terraform.workspace == "default" ? "dev" : terraform.workspace
 }
 module "eventrule" {
     source = "../modules/eventrule"
     rulename = "mytest_terraform_rule"
+    workspace = terraform.workspace == "default" ? "dev" : terraform.workspace
+    
 }
