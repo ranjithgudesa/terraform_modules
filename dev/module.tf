fde77a2b460c9e2b70a22a6f291363620c22c16d
ranjithgudesa <101344279+ranjithgudesa@users.noreply.github.com>
2024-12-17 12:32:39 +0530

    Update module.tf

--- dev/module.tf
+++ dev/module.tf
@@ -1,8 +1,8 @@
 module "s3" {
     source = "../modules/s3"
-    s3_bucket_name = "mytest_terraform_bucket"  
+    s3_bucket_name = "mydev_terraform_bucket"  
 }
 module "eventrule" {
     source = "../modules/eventrule"
-    rulename = "mytest_terraform_rule"
+    rulename = "mydev_terraform_rule"
 }
